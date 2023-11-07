1bb36f8ddad9e8fa16145000195cec6be159db71
Rodrigo Gonçalves <rodrigogoncalves2005t@gmail.com>
2023-11-06 23:33:11 -0300

    handleNext no botao de login

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,13 +1,21 @@
+import { useRouter } from "next/navigation"
 import { Button } from "../button/Button"
 import { Logo } from "../logo/Logo"
 import { ElementHeader, HeaderContainer } from "./Header.style"
 
 export const Header = () => {
+
+  const router = useRouter()
+
+  const handleNext = () => {
+    router.push('/login')
+  }
+
   return (
     <ElementHeader>
       <HeaderContainer>
         <Logo />
-        <Button>Login</Button>
+        <Button onClick={handleNext}>Login</Button>
       </HeaderContainer>
     </ElementHeader>
   )
