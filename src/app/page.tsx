8db8520223df8c894d4617ca4d6d32867bc5ce6b
Danilo Mendonça <danilomendonca08@gmail.com>
2023-11-06 23:36:58 -0300

    teste-pagina-cadastro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,13 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import { GlobalStyles } from '@/styles/GlobalStyles'
 import Home from './home/page'
+import SignUp from './signUp/page'
 
 export default function home() {
   return (
       <>
       <GlobalStyles />
-      <Home />
+      <SignUp />
       </>
   )
 }
