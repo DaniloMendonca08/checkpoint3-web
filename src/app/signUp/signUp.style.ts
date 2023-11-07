4cfc2c875c1e7da4c7fe5ca0e6c7768e4997342d
Arthur <arthurwrx@outlook.com>
2023-11-07 12:34:30 -0300

    Arquivo com o nome corrigido

---
+++ src/app/signUp/signUp.style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+import { colors } from "@/styles/Colors"
+
+export const StyledDivSignUp = styled.div`
+    display: flex;
+    margin-bottom:15px ;
+`
+
+export const StyledLabelSignUp = styled.label`
+    font-size: 20px;
+    padding-right: 15px;
+    color: ${colors.text.dark};
+`
