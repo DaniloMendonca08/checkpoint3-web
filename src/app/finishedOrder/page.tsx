07ddb1c2b972bcb5e0459cb54599fa0d8a94eedd
Rodrigo Gonçalves <rodrigogoncalves2005t@gmail.com>
2023-11-06 21:44:43 -0300

    criado a pagina de pedido concluido

---
+++ src/app/finishedOrder/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { Layout } from "../components/layout/Layout";
+import { Title } from "../components/title/Title";
+
+export default function finishedOrder () {
+    return (
+        <Layout>
+            <Title>Obrigado pelo pedido!</Title>
+        </Layout>
+    )
+}
