187757ee0a4fa819008b1f56a6355cbabd6f1f00
Danilo Mendonça <danilomendonca08@gmail.com>
2023-11-08 20:07:12 -0300

    ajuste-mostra-ultimo-pedido

--- src/app/finishedOrder/page.tsx
+++ src/app/finishedOrder/page.tsx
@@ -1,12 +1,17 @@
 "use client"
 
+import { useContext, useEffect, useState } from "react";
 import { Layout } from "../components/layout/Layout";
 import { Title } from "../components/title/Title";
+import OrderContext from "@/contexts/OrderContext";
 
 export default function FinishedOrder () {
+    const { pizzaOrder } = useContext(OrderContext)
+
     return (
         <Layout>
             <Title>Obrigado pelo pedido!</Title>
+            <Title>Últimos pedidos: {pizzaOrder.item.name}</Title>
         </Layout>
     )
 }
